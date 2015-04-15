8f899d00f34aa6e168d6ed715eb086e8038db960
KAJIWARAYutaka <20511091yk@grad.tama.ac.jp>
2015-04-16 01:09:45 +0900

    データベース作成・設定ファイル名変更・設定追加

---
+++ UserRegister/src/main/java/UserRegister/DBConfig.java
@@ -0,0 +1,36 @@
+package UserRegister;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by KAJIWARAYutaka on 2015/04/18.
+ */
+@Configuration
+public class DBConfig {
+
+    @Autowired
+    DataSourceProperties properties;
+    DataSource dataSource;
+
+    @Bean(destroyMethod = "close")
+    DataSource realDataSource() {
+        String url;
+        String username;
+        String password;
+
+        url = this.properties.getUrl();
+        username = this.properties.getUsername();
+        password = this.properties.getPassword();
+        DataSourceBuilder factory = DataSourceBuilder.create()
+                .driverClassName(this.properties.getDriverClassName())
+                .url(url).username(username).password(password);
+        this.dataSource = factory.build();
+        return this.dataSource;
+    }
+}
