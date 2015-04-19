e41b369af949cd0215dfe9293ac9f256ccf2f705
KAJIWARA.Yutaka <20511091yk@grad.tama.ac.jp>
2015-04-19 16:40:49 +0900

    データ登録を実装

--- UserRegister/src/main/java/UserRegister/UserRegisterApplication.java
+++ UserRegister/src/main/java/UserRegister/UserRegisterApplication.java
@@ -1,12 +1,27 @@
 package UserRegister;
 
+import UserRegister.Repository.UserRepository;
+import UserRegister.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserRegisterApplication {
+public class UserRegisterApplication implements CommandLineRunner{
+    @Autowired
+    UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(UserRegisterApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Student student = new Student();
+        student.setId(20511091);
+        student.setName("かじわら ゆたか");
+        userRepository.save(student);
+
     }
 }
