66a91fb080aac1a7fba152fc8024246095c016c8
KAJIWARA.Yutaka <20511091yk@grad.tama.ac.jp>
2015-04-20 23:06:43 +0900

    DBに対するCRUDを実装

--- UserRegister/src/main/java/UserRegister/UserRegisterApplication.java
+++ UserRegister/src/main/java/UserRegister/UserRegisterApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class UserRegisterApplication implements CommandLineRunner{
     @Autowired
@@ -19,9 +21,30 @@ public class UserRegisterApplication implements CommandLineRunner{
     @Override
     public void run(String... strings) throws Exception {
         Student student = new Student();
+        //Create
         student.setId(20511091);
         student.setName("かじわら ゆたか");
         userRepository.save(student);
+        //Select(Read)
+        student = userRepository.findOne(20511091);
+        System.out.println("User ID = " + student.getId());
+        System.out.println("User Name = " + student.getName());
+
+        //Update
+        student.setName("梶原 裕");
+        userRepository.save(student);
+        //Select
+        student = userRepository.findOne(20511091);
+        System.out.println("User ID = " + student.getId());
+        System.out.println("User Name = " + student.getName());
+
+        //Delete
+        System.out.println("Delete User ? y/N");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        if(answer.equals("y")){
+            userRepository.delete(student);
+        }
 
     }
 }
