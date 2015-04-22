78714c745823a0ad8cc26aeda248acb45d9ac49d
KAJIWARA Yutaka <kajiwara-y@tama.ac.jp>
2015-04-22 09:04:23 +0900

    ユーザー一覧表示用のAPIを作成

--- UserRegisterRest/src/main/java/UserRegister/Resource/UserResource.java
+++ UserRegisterRest/src/main/java/UserRegister/Resource/UserResource.java
@@ -1,7 +1,22 @@
 package UserRegister.Resource;
 
-/**
- * Created by kajiwarayutaka on 2015/04/22.
- */
+import UserRegister.Repository.UserRepository;
+import UserRegister.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class UserResource {
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value="students",method = RequestMethod.GET)
+    List<Student> studentList(){
+        return userRepository.findAll();
+    }
 }
