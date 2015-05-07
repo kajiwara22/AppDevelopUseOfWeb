d4be42c3c689ac9477d8d1307fc8e2f03aa7d01f
KAJIWARA Yutaka <20511091yk@grad.tama.ac.jp>
2015-05-08 08:36:13 +0900

    作業中

--- UserRegisterMVC/src/main/java/UserRegister/Controller/ShowPageController.java
+++ UserRegisterMVC/src/main/java/UserRegister/Controller/ShowPageController.java
@@ -6,26 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by KAJIWARAYutaka on 2015/05/02.
  */
 @Controller
 @RequestMapping(value="/")
-public class UserRegistController {
-    @Autowired
-    UserRepository userRepository;
-
+public class ShowPageController {
     @RequestMapping(method = RequestMethod.GET)
     public String pageShow(){
        return "index";
-    }
-
-    @RequestMapping(value="users" ,method = RequestMethod.GET)
-    public ModelAndView usersShow(Model model){
-        model.addAttribute("users",userRepository.findAll());
-
-        return new ModelAndView("userList","model",model);
     }
 }
