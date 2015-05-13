cd5b9dbd604b375e77ad31e77246fd8d93ba273a
KAJIWARA Yutaka <kajiwara-y@tama.ac.jp>
2015-05-13 22:30:51 +0900

    実装開始

---
+++ RecordWriter/src/main/java/recordwriter/model/record.java
@@ -0,0 +1,12 @@
+package recordwriter.model;
+
+import lombok.Data;
+
+/**
+ * Created by kajiwarayutaka on 2015/05/13.
+ */
+@Data
+public class record {
+    private String title;
+    private String record;
+}
