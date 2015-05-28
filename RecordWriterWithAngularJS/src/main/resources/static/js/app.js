4aa9592336dc1b864d5178adf57ac10b5ad3bc10
KAJIWARA Yutaka <20511091yk@grad.tama.ac.jp>
2015-05-29 08:42:34 +0900

    作成中

---
+++ RecordWriterWithAngularJS/src/main/resources/static/js/app.js
@@ -0,0 +1,6 @@
+(function(){
+    'use strict';
+    angular.module('recordWriter').config(function($locationProvider){
+        $locationProvider.html5Mode(true);
+    });
+});
---
+++ RecordWriterWithAngularJS/src/main/resources/static/js/controller/indexController.js
@@ -0,0 +1,6 @@
+(function(){
+    'use strict';
+    angular.module('recordWriter').controller('indexController',function($scope){
+
+    });
+});
